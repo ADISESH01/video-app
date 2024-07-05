f8fe03e9dc36ec6ee77f53cb098fb16c87549419
ADISESH01 <adisesh018@gmail.com>
2024-07-05 12:27:57 +0530

    Initialed

---
+++ src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+import Home from './components/home/Home';
+import NewsComponent from './components/news/NewsComponents';
+import Video from './components/video/Video';
+function App() {
+  return (
+    <header className="App-header">
+      <div className="App">
+    <h1>Adi</h1>
+    <Home/>
+    <Video/>
+    <NewsComponent/>
+  </div></header>
+  );
+}
+export default App;
